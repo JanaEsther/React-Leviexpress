ffc15124420c5c1ba26fc085f77e88ecd32e00c1
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-12-15 21:18:41 +0100

    rezervation-confirmation

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,22 +1,57 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
 import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
-  const[journey,setJourney]= useState(null)
+  const navigate = useNavigate();
 
-  const handleJourneyChange = (journey)=>{
+  const handleClick = () => {
+    navigate(-1);
+  };
+
+  const [journey, setJourney] = useState(null);
+
+  const handleJourneyChange = (journey) => {
     setJourney(journey);
-  }
+  };
 
- 
+  const handleBuy = () => {
+    fetch('https://apps.kodim.cz/daweb/leviexpress/api/reservation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'create',
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const reservationId = data.reservationId;
+        console.log(reservationId);
+        navigate(`reservation/${reservationId}`);
+      });
+  };
 
   return (
-    <main>
-      <JourneyPicker onJourneyChange={handleJourneyChange} />
-      {journey && <SelectedSeat number={journey.autoSeat} />}
-      {journey && (<JourneyDetail journey={journey}/>)}
-    </main>
+    <>
+      <main>
+        <JourneyPicker onJourneyChange={handleJourneyChange} />
+        {journey && <SelectedSeat number={journey.autoSeat} />}
+        <div className="controls container">
+          <button className="btn btn--big" onClick={handleBuy} type="button">
+            Rezervovat
+          </button>
+          <button className="btn btn__back" onClick={handleClick} type="button">
+            Zpět
+          </button>
+        </div>
+        {journey && <JourneyDetail journey={journey} />}
+      </main>
+    </>
   );
 };
